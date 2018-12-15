bf63a0057eb0cf43abb4dee85a868639a5ca6960
aokitashipro <webmocha2@gmail.com>
2018-12-15 10:00:34 +0900

    splfileobject

--- app/Http/Controllers/BookingcurvesController.php
+++ app/Http/Controllers/BookingcurvesController.php
@@ -7,8 +7,9 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 
 use App\Bookingcurve;
-use Excel;
 
+//useしないと 自動的にnamespaceのパスが付与されるのでuse
+use SplFileObject;
 
 class BookingcurvesController extends Controller
 {
@@ -30,9 +31,9 @@ class BookingcurvesController extends Controller
 
     public function index()
     {
-        $data = [];
-        $data['bookingcurve'] = $this->bookingcurve->all();
-        return view('welcome', $data);
+        // $data = [];
+        // $data['bookingcurve'] = $this->bookingcurve->all();
+        return view('welcome');
     }
 
     /**
@@ -43,26 +44,42 @@ class BookingcurvesController extends Controller
      */
     public function import(Request $request)
     {
-        $file = $request->file('csv_file');
-        $reader = Excel::load($file->getRealPath())->get();
+    // setlocaleを設定
+    setlocale(LC_ALL, 'ja_JP.UTF-8');
 
-        $rows = $reader->toArray();
+    // アップロードしたファイルを取得
+    // 'csv_file' はCSVファイルインポート画面の inputタグのname属性
+    $uploaded_file = $request->file('csv_file');
 
-        // echo '<pre>';
-        // var_dump($rows);
-        // echo '</pre>';
+    // アップロードしたファイルの絶対パスを取得
+    $file_path = $request->file('csv_file')->path($uploaded_file);
 
-         foreach ($rows as $row){
-             if (!isset($row['予約サイト名'])) {
-                 return redirect()->back();
-             }
+    $file = new SplFileObject($file_path);
+    $file->setFlags(SplFileObject::READ_CSV);
 
-             $record = $this->bookingcurve->firstOrNew(['ota' => $row['予約サイト名']]);
-             $record->name = $row['予約サイト名'];
-             $record->save();
-         }
-         return redirect()->action('BookingcurvesController@index');
+    //dd($file);
+
+    $row_count = 1;
+    foreach ($file as $row)
+    {
+        // 1行目のヘッダーは取り込まない
+        if ($row_count > 1)
+        {
+            $id = mb_convert_encoding($row[0], 'UTF-8', 'SJIS');
+            $name = mb_convert_encoding($row[1], 'UTF-8', 'SJIS');
+            $age = mb_convert_encoding($row[2], 'UTF-8', 'SJIS');
+
+            var_dump($id);
+            var_dump($name);
+            var_dump($age);
+
+            // ここで値をデータベースに保存したりする
+
+        }
+        $row_count++;
     }
 
+
+    }
 
 }
